f1913aa3b042aaa4ebfcda073c7d1e4a1310cf2c
Danniel Navas <dannielnavas@gmail.com>
2024-09-23 21:29:37 -0500

    ✨ Implementar formulario de configuración con validación y servicio de enlaces

--- src/app/domains/public/components/form/form.component.ts
+++ src/app/domains/public/components/form/form.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { LinksService } from '../../../shared/services/links.service';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './form.component.html',
-  styleUrl: './form.component.css'
+  styleUrl: './form.component.css',
 })
 export class FormComponent {
+  formLink!: FormGroup;
 
+  private formBuilder!: FormBuilder;
+  private linksService = inject(LinksService);
+
+  ngOnInit() {
+    this.initForm();
+  }
+
+  initForm() {
+    this.formLink = this.formBuilder.group({
+      url: ['', [Validators.required]],
+      identifier: ['', [Validators.required]],
+      key: ['', [Validators.required]],
+    });
+  }
+
+  onSubmit() {
+    if (this.formLink.invalid) {
+      this.formLink.reset();
+      return;
+    }
+    this.linksService.createLink(this.formLink.value).subscribe({
+      next: () => {
+        this.formLink.reset();
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
+  }
 }
